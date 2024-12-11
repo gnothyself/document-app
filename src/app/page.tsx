feff4484fe544c95e9152959b0b141cdc7013931
gnothyself <seanpatrick0x@gmail.com>
2024-12-11 11:59:04 -0700

    small message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import Link from 'next/link';
 
 const Home = () => {
   return (
-    <div className='flex min-h-screen items-center justify-center gap-2'>
-      Click
-      <Link href={'/documents'} className='text-blue-600 underline'>
-        here
-      </Link>
-      to go to documents page
+    <div className='flex min-h-screen items-center justify-center gap-2 flex-col'>
+      <div className='text-2xl font-bold'>Welcome to the Home Page</div>
+      <div className='flex gap-2'>
+        Click
+        <Link href={'/documents'} className='text-blue-600 underline'>
+          here
+        </Link>
+        to go to documents page
+      </div>
     </div>
   );
 };
